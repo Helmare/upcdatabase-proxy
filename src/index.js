af60d7c633ac3b4e640ee603cba33f5a6b902e25
Helmare <h3lmare@gmail.com>
2024-08-17 10:38:18 -0500

    Created a basic proxy that formats headers for https://api.upcdatabase.org.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import axios from 'axios';
+
+// Get environment variables.
+dotenv.config();
+const host = process.env.UPCD_HOST || 'https://api.upcdatabase.org';
+const port = process.env.PORT || 3000;
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('{"hello": "world"}');
+app.use(async (req, res) => {
+  try {
+    delete req.headers.host;
+    if (req.headers['authorization']) {
+      req.headers['Authorization'] = req.headers['authorization'];
+      delete req.headers['authorization'];
+    }
+
+    console.log(`${req.method} ${host}${req.url}`);
+    const response = await axios({
+      url: `${host}${req.url}`,
+      method: req.method,
+      headers: req.headers,
+      data: req.body
+    });
+
+    res.statusCode = response.status;
+    res.statusMessage = response.statusText;
+    res.send(response.data);
+  }
+  catch (error) {
+    res.status(500).send({ success: false, error: error.message });
+  }
 });
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`https://api.database.org proxy running on port ${port}`);
 });
